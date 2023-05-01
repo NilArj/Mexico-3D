d606212a180c03b752efb41a4d2576ca52c37e3d
NilArj <nnilviaa@gmail.com>
2023-05-01 13:04:35 -0600

    stadium complete

--- src/pages/Stadium.jsx
+++ src/pages/Stadium.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Stage, OrbitControls, Gltf } from "@react-three/drei";
 
 const Stadium = () => {
   return (
-    <div>Stadium</div>
-  )
-}
+    <div className="content-section">
+      <Canvas shadows camera={{ position: [-1, 6, 8], fov: 35 }}>
+        <Stage
+          intensity={0.3}
+          preset="rembrandt"
+          adjustCamera={1.1}
+          environment="night"
+        >
+          <Gltf castShadow receiveShadow src="camp_nou_stadium.glb" />
+        </Stage>
+        <OrbitControls
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 1.9}
+          makeDefault
+        />
+      </Canvas>
+    </div>
+  );
+};
 
-export default Stadium
+export default Stadium;
