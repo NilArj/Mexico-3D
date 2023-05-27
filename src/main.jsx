3043cc74117e1c18aeaf2bb5e06e5d987f16637a
NilArj <nnilviaa@gmail.com>
2023-05-26 20:17:17 -0600

    frame on process

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,12 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Error from "./pages/Error";
 import Loader from "./components/Loader";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <Error />,
-  },
-]);
+import Home from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Suspense fallback={<Loader />}>
-      <RouterProvider router={router} />
+      <Home />
     </Suspense>
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import { Canvas, useLoader } from "@react-three/fiber";
-import { useState } from "react";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import {
-  Text,
-  Float,
-  Environment,
-  MeshReflectorMaterial,
-} from "@react-three/drei";
+import { Text, Float, Environment, useCursor } from "@react-three/drei";
+import { useRoute, useLocation } from "wouter";
+import { easing } from "maath";
+import getUuid from "uuid-by-string";
 
 import bgImage from "../images/chichen3.jpg";
 import image1 from "../images/diademuertos.jpg";
 import image2 from "../images/mariachi.jpg";
 import image4 from "../images/chichen2.jpg";
+
+const GOLDENRATIO = 1.61803398875;
 
 const CustomTexts = () => {
   return (
@@ -71,10 +71,36 @@ const CustomBackground = () => {
   );
 };
 
-const Images = ({ url, position }) => {
-  const [hovered, setHovered] = useState(false);
+const ImageFrame = ({ url, position, c = new THREE.Color() }) => {
   const texture = useLoader(THREE.TextureLoader, url);
+  const frame = useRef();
+  const [hovered, setHovered] = useState(false);
   const frameColor = hovered ? "#537188" : "#f2f1ee";
+  const [, params] = useRoute("/item/:id");
+  const [rnd] = useState(() => Math.random());
+  const name = getUuid(url);
+  const isActive = params?.id === name;
+  useCursor(hovered);
+  useFrame((state, dt) => {
+    frame.current.material.zoom =
+      2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2;
+    easing.damp3(
+      frame.current.scale,
+      [
+        0.85 * (!isActive && hovered ? 0.85 : 1),
+        0.9 * (!isActive && hovered ? 0.905 : 1),
+        1,
+      ],
+      0.1,
+      dt
+    );
+    easing.dampC(
+      frame.current.material.color,
+      hovered ? "blue" : "white",
+      0.1,
+      dt
+    );
+  });
 
   return (
     <Float
@@ -83,12 +109,20 @@ const Images = ({ url, position }) => {
       floatIntensity={0.2}
       floatingRange={[0.1, -0.1]}
     >
-      <group position={position}>
+      <group>
         {/* image */}
-        <mesh castShadow receiveShadow>
+        <mesh
+          name={name}
+          ref={frame}
+          onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}
+          onPointerOut={() => setHovered(false)}
+          scale={[1, GOLDENRATIO, 0.05]}
+          position={[position[0], position[1], position[2]]}
+        >
           <planeGeometry args={[1, 1]} />
           <meshStandardMaterial map={texture} />
         </mesh>
+
         {/* frame */}
         <mesh
           castShadow
@@ -100,6 +134,7 @@ const Images = ({ url, position }) => {
           <planeGeometry args={[1.08, 1.08]} />
           <meshStandardMaterial color={frameColor} />
         </mesh>
+
         {/* shadow */}
         <mesh
           position={[0, -1.4, 0]}
@@ -107,8 +142,64 @@ const Images = ({ url, position }) => {
           receiveShadow
         >
           <planeGeometry args={[1.4, 1.4]} />
-          <meshStandardMaterial color={"#000000"} transparent opacity={0.3} />
+          <meshStandardMaterial color="#000000" transparent opacity={0.3} />
         </mesh>
+      </group>
+    </Float>
+  );
+};
+
+const Images = ({
+  images,
+  q = new THREE.Quaternion(),
+  p = new THREE.Vector3(),
+}) => {
+  const ref = useRef();
+  const clicked = useRef();
+  const [, params] = useRoute("/item/:id");
+  const [, setLocation] = useLocation();
+
+  useEffect(() => {
+    clicked.current = ref.current.getObjectByName(params?.id);
+    if (clicked.current) {
+      clicked.current.parent.updateWorldMatrix(true, true);
+      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25));
+      clicked.current.parent.getWorldQuaternion(q);
+    } else {
+      p.set(0, 0, 5.5);
+      q.identity();
+    }
+  });
+
+  useFrame((state, dt) => {
+    easing.damp3(state.camera.position, p, 0.4, dt);
+    easing.dampQ(state.camera.quaternion, q, 0.4, dt);
+  });
+
+  return (
+    <Float
+      speed={1}
+      rotationIntensity={0.1}
+      floatIntensity={0.2}
+      floatingRange={[0.1, -0.1]}
+    >
+      <group
+        ref={ref}
+        onClick={(e) => (
+          e.stopPropagation(),
+          setLocation(
+            clicked.current === e.object ? "/" : "/item/" + e.object.name
+          )
+        )}
+        onPointerMissed={() => setLocation("/")}
+      >
+        {images.map((image) => (
+          <ImageFrame
+            key={image.item}
+            url={image.url}
+            position={image.position}
+          />
+        ))}
       </group>
     </Float>
   );
@@ -145,10 +236,7 @@ const Home = () => {
       <CustomTexts />
       <ambientLight intensity={0.2} />
       <spotLight position={[10, 15, 10]} angle={0.3} penumbra={3} castShadow />
-      {images.map((item) => (
-        <Images key={item.url} url={item.url} position={item.position} />
-      ))}
-
+      <Images images={images} />
       <Environment preset="city" />
     </Canvas>
   );
