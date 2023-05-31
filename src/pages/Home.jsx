9c1f3b76775d01f51be5aa968a289871c8f224ce
NilArj <nnilviaa@gmail.com>
2023-05-31 17:49:34 -0600

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { useRoute, useLocation } from "wouter";
 import { easing } from "maath";
 import getUuid from "uuid-by-string";
 
-import bgImage from "../images/chichen3.jpg";
+import bgImage from "../images/wall2.jpg";
 import image1 from "../images/diademuertos.jpg";
 import image2 from "../images/mariachi.jpg";
 import image4 from "../images/chichen2.jpg";
@@ -21,16 +21,16 @@ const CustomTexts = () => {
         maxWidth={5}
         anchorX="center"
         anchorY="top"
-        position={[0, 3.6, 0]}
+        position={[0, 3.8, 0]}
         fontSize={0.15}
       >
         Nilvia Arjona Project
       </Text>
       <Float
-        speed={2} // Animation speed, defaults to 1
-        rotationIntensity={0.6} // XYZ rotation intensity, defaults to 1
-        floatIntensity={0.5} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-        floatingRange={[0.1, -0.1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        speed={2}
+        rotationIntensity={0.5}
+        floatIntensity={0.5}
+        floatingRange={[0.1, -0.1]}
       >
         <Text
           curveRadius={-8}
@@ -40,7 +40,7 @@ const CustomTexts = () => {
           maxWidth={3}
           anchorX="center"
           anchorY="top"
-          position={[0.1, 2.6, 0]}
+          position={[0.1, 3, 0]}
           fontSize={1}
         >
           MEXICO
@@ -60,10 +60,10 @@ const CustomBackground = () => {
     <Float
       speed={1} // Animation speed, defaults to 1
       rotationIntensity={0.1} // XYZ rotation intensity, defaults to 1
-      floatIntensity={0.2} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+      floatIntensity={0.2} // /down float intensity, works like a multiplier with floatingRange,defaults to 1
       floatingRange={[-0.1, 0.1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
     >
-      <mesh scale={[20, 9.7, 1]} position={[0, 0, -1]}>
+      <mesh scale={[25, 9.8, 1]} position={[0, 0, -1]}>
         <meshBasicMaterial map={texture} />
         <planeGeometry args={[1, 1]} />
       </mesh>
@@ -96,7 +96,7 @@ const ImageFrame = ({ url, c = new THREE.Color(), ...props }) => {
     );
     easing.dampC(
       frame.current.material.color,
-      hovered ? "yellow" : "white",
+      hovered ? "#f6a700" : "#f2f1ee",
       0.1,
       dt
     );
@@ -113,7 +113,7 @@ const ImageFrame = ({ url, c = new THREE.Color(), ...props }) => {
       >
         <boxGeometry />
         <meshStandardMaterial
-          color="#151515"
+          color="#537188"
           metalness={0.5}
           roughness={0.5}
           envMapIntensity={2}
@@ -188,7 +188,7 @@ const Home = () => {
   const images = [
     // Left
     {
-      position: [-1.8, 0, 2],
+      position: [-1.75, -0.09, 2],
       rotation: [0, Math.PI / 20, 0],
       url: image2,
       item: 2,
@@ -202,7 +202,7 @@ const Home = () => {
     },
     // Right
     {
-      position: [1.8, 0, 2],
+      position: [1.75, -0.09, 2],
       rotation: [0, -Math.PI / 20, 0],
       url: image4,
       item: 4,
